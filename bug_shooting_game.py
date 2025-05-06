f2d4b1256e1896e80a538d759d46da882cc09be4
weardonoob <sharma.alexander@icloud.com>
2025-05-06 18:00:34 +0200

    Add files via upload

--- bug_shooting_game.py
+++ bug_shooting_game.py
@@ -7,10 +7,14 @@ TITLE = "bug_shooting_game"
 ship = Actor("galaga")
 ship.pos = WIDTH / 2, HEIGHT -50
 
+game_state = "start"
+ship.death = False
+ship.countdown = 90
+score = 0
 bullets = []
 bugs = []
 xp = 100
-direction = -1
+direction = 1
 move = True
 
 for i in range(4):
@@ -22,14 +26,23 @@ for i in range(4):
 
 def draw():
     screen.blit("space_background",(0,0))
-    ship.draw()
-    for i in range(4):
-       bugs[i].draw()
-    for i in bullets:
-       i.draw()
+    if game_state == "start":
+       screen.draw.text("press space to start \n kill enemies to win \n if you get hit you will be revived",(150,200))
+    if game_state == "over":
+       screen.draw.text("yay you have killed all the enemies",(150,200))
+    if game_state == "play":
+      
+      if not ship.death:
+         ship.draw()
+      for i in range(len(bugs)):
+         bugs[i].draw()
+      for i in bullets:
+         i.draw()
+
 def update():
-   global direction
-   if (bugs[0].x < 0 and bugs[-1].x > WIDTH - 30) and len(bugs) > 0:
+   global direction, score, game_state
+   print(ship.countdown)
+   if (bugs[0].x < 30 or bugs[-1].x > WIDTH - 30) and len(bugs) > 0:
      direction = direction * (-1)
      for bug in bugs:
       bug.x += direction * 2
@@ -40,21 +53,39 @@ def update():
    for i in bugs:
         i.y += 1
         i.x += direction * 2
+        if i.colliderect(ship):
+           ship.death = True
+        for b in bullets:
+           if b.colliderect(i):
+              bullets.remove(b)
+              bugs.remove(i)
+              score += 1
+   
    for i in bullets:
       i.y -= 2
-
+   if ship.death:
+     ship.countdown -= 1
+   if ship.countdown == 0:
+       ship.death = False
+       ship.countdown = 90
+   if len(bugs) == 0:
+      game_state = "over"
 
 def on_key_down(key):
-    global bullets
+    global bullets, game_state
     print(key)
-    if key == keys.LEFT and ship.x > 40:
-       ship.x -= 10
-    if key == keys.RIGHT:
-        if ship.x < WIDTH - 40:
-         ship.x += 10
-    if key == keys.SPACE:
-       bullet = Actor("bullet")
-       bullet.pos = ship.x,ship.y - 40
-       bullets.append(bullet)
+    if not ship.death and game_state == "play":
+         
+      if key == keys.LEFT and ship.x > 40:
+         ship.x -= 10
+      if key == keys.RIGHT:
+         if ship.x < WIDTH - 40:
+            ship.x += 10
+      if key == keys.SPACE:
+         bullet = Actor("bullet")
+         bullet.pos = ship.x,ship.y - 40
+         bullets.append(bullet)
+    if key == keys.SPACE and game_state == "start":
+       game_state = "play"
 
 pgzrun.go()
--- shooting_game_horizontal.py
+++ shooting_game_horizontal.py
@@ -4,28 +4,44 @@ WIDTH = 800
 HEIGHT = 500
 TITLE = "horizontal_shooting_game"
 
-ship = Actor("captain_america")
-ship.pos = WIDTH - 50, HEIGHT / 2
-bug = Actor("venom")
-bug.pos = 0,random.randint(40,HEIGHT - 40)
+venoms = []
+shields = []
+cap = Actor("captain_america")
+cap.pos = WIDTH - 50, HEIGHT / 2
+for i in range(3):
+   ven = Actor("venom")
+   ven.pos = 0,random.randint(40,HEIGHT - 40)
+   venoms.append(ven)
 
 def draw():
     screen.blit("space_background",(0,0))
-    ship.draw()
-    bug.draw()
+    cap.draw()
+ #   ven.draw()
+    for i in shields:
+       i.draw()
+    for i in venoms:
+       i.draw()
 def update():
-   bug.x += 1
-   if bug.x > WIDTH:
-      bug.x = -20
+   for i in venoms:
+      i.x += 1
+
+   if ven.x > WIDTH:
+      ven.x = -20
+   for i in shields:
+      i.x -= 2
 
 
 def on_key_down(key):
+    global shields
     print(key)
-    if key == keys.UP and ship.y > 40:
-       ship.y -= 10
+    if key == keys.UP and cap.y > 40:
+       cap.y -= 10
     if key == keys.DOWN:
-        if ship.y < HEIGHT - 40:
-         ship.y += 10
-
+        if cap.y < HEIGHT - 40:
+         cap.y += 10
+    if key == keys.SPACE:
+       shield = Actor("cap_shield")
+       shield.pos = cap.x - 40,cap.y
+       shields.append(shield)
 
 pgzrun.go()
